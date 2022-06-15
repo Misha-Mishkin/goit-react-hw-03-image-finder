a15a8aaf66d989cab867b70f5de94a0ceb244469
Mihail Mishkin <mihail.mishkin89@gmail.com>
2022-06-15 17:08:10 +0300

    Позиционирование Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,20 +1,19 @@
 import { Component } from 'react';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import { Bars } from 'react-loader-spinner';
+import s from '../Loader/Loader.module.css'
 
 export default class Loader extends Component {
   render() {
     return (
-      <Bars
+      <div className={s.Loader}>
+        <Bars
         color="#3f51b5"
         height={50}
         width={50}
-        ariaLabel="loading"
-        style={{
-          marginLeft: 'auto',
-          marginRight: 'auto',
-        }}
+        ariaLabel="loading"        
       />
+      </div>
     );
   }
 }
